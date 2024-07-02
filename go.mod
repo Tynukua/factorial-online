272409fbc058f1b94642d82161ff06248b5568b9
Tynuk <bowsworddv@gmail.com>
2024-07-02 12:53:16 +0300

    createed main handler and middlware without calculating

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Tynukua/factorial-online
+
+go 1.22.4
+
+require github.com/julienschmidt/httprouter v1.3.0
---
+++ main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	fmt.Fprint(w, "Welcome!\n")
+}
+
+type CalculateRequest struct {
+	A *int `json:"a"`
+	B *int `json:"b"`
+}
+
+func CalculateCheckInputMiddleware(next httprouter.Handle) httprouter.Handle {
+	return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+		params := CalculateRequest{}
+		if err := json.NewDecoder(r.Body).Decode(&params); err != nil {
+			w.Write([]byte("Invid JSON resourse"))
+			return
+		}
+		if params.A == nil || params.B == nil {
+			w.Write([]byte("Params a and b must be provided"))
+			return
+		}
+		if *params.A < 0 || *params.B < 0 {
+			w.Write([]byte("Value must be non-negative"))
+			return
+		}
+
+		ctx := context.WithValue(r.Context(), "CalculateData", params)
+		r = r.WithContext(ctx)
+		next(w, r, ps)
+	}
+}
+func Calculate(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	params := r.Context().Value("CalculateData").(CalculateRequest)
+	var a, b int = *params.A, *params.B
+
+	/// CALCULATE
+
+	var response map[string]int = map[string]int{"a!": a, "b!": b}
+	responsedata, err := json.Marshal(response)
+	if err != nil {
+		http.Error(w, "internal error", http.StatusInternalServerError)
+		return
+	}
+	w.Write(responsedata)
+}
+
+func main() {
+	router := httprouter.New()
+	router.GET("/", Index)
+	router.POST("/calculate", CalculateCheckInputMiddleware(Calculate))
+	log.Println("Server started on port 8080")
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
