e56e56526f5d393a4bfd88e92b5e002a01dd73de
VD <bowsworddv@gmail.com>
2024-07-09 22:12:36 +0300

    hide wg in do

--- internal/calculator/service/service.go
+++ internal/calculator/service/service.go
@@ -3,6 +3,7 @@ package service
 import (
 	"context"
 	"github.com/Tynukua/factorial-online/internal/calculator"
+	"sync"
 )
 
 type AsyncService struct {
@@ -10,9 +11,11 @@ type AsyncService struct {
 }
 
 func (a AsyncService) Do(ctx context.Context, fs []func()) error {
-
+	wg := sync.WaitGroup{}
+	wg.Add(len(fs))
 	for _, f := range fs {
-		go f()
+		go func() { f(); wg.Done() }()
 	}
+	wg.Wait()
 	return nil
 }
--- internal/calculator/test/calculator_test.go
+++ internal/calculator/test/calculator_test.go
@@ -6,7 +6,6 @@ import (
 	"github.com/Tynukua/factorial-online/internal/calculator/mysql"
 	"github.com/Tynukua/factorial-online/internal/calculator/service"
 	"log"
-	"sync"
 	"testing"
 )
 
@@ -14,19 +13,15 @@ func TestCalculatorService(t *testing.T) {
 	var a service.AsyncService
 	m := mysql.NewMysqlCalculator("shrug", mathematics.MathCalculator{})
 	ctx := context.TODO()
-	wg := sync.WaitGroup{}
 	f := func() {
 		log.Println(m.Factorial(ctx, 5555))
-		wg.Done()
 	}
 	g := func() {
 		log.Println(m.Factorial(ctx, 6666))
-		wg.Done()
 	}
 	fs := []func(){f, g}
-	wg.Add(len(fs))
 	err := a.Do(ctx, fs)
-	wg.Wait()
+
 	if err != nil {
 		t.Error(err)
 	}
