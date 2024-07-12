259d70f6a97e90d4d9677e674004b73375ad4514
VD <bowsworddv@gmail.com>
2024-07-12 13:00:34 +0300

    del dublicate of test

--- cmd/webapp/main_test.go
+++ cmd/webapp/main_test.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"github.com/Tynukua/factorial-online/internal/config"
 	"github.com/Tynukua/factorial-online/internal/util"
-	"github.com/Tynukua/factorial-online/internal/webservices"
 	"math/big"
 	"testing"
 )
@@ -41,32 +39,6 @@ func TestMulRangeParrallel(t *testing.T) {
 		got := util.MulRangeParallel(c.a, c.b, 2)
 		if got.Cmp(c.expected) != 0 {
 			t.Fatalf("MulRange(%d, %d) = %d, want %d", c.a, c.b, got, c.expected)
-		}
-	}
-}
-
-type FactorialCase struct {
-	a         int
-	b         int
-	expecteda *big.Int
-	expectedb *big.Int
-}
-
-func TestDoubleFactorial(t *testing.T) {
-	s := webservices.NewFactorialService(config.Config{})
-
-	cases := []FactorialCase{
-		{1, 2, big.NewInt(1), big.NewInt(2)},
-		{1, 2, big.NewInt(1), big.NewInt(2)},
-		{1, 5, big.NewInt(1), big.NewInt(120)},
-		{5, 1, big.NewInt(120), big.NewInt(1)},
-		{11, 11, big.NewInt(39916800), big.NewInt(39916800)},
-	}
-
-	for _, c := range cases {
-		gota, gotb := s.DoubleFactorial(c.a, c.b)
-		if gota.Cmp(c.expecteda) != 0 || gotb.Cmp(c.expectedb) != 0 {
-			t.Fatalf("DoubleFactorial(%d, %d) = %d, %d, want %d, %d", c.a, c.b, gota, gotb, c.expecteda, c.expectedb)
 		}
 	}
 }
